462be8546682c2f39975b9a6cdb89d9113f63efc
ckartik <k5chopra@uwaterloo.ca>
2022-04-09 16:11:01 -0400

    Adds some basic strcutures.

--- contracts/PeerReviewDAO.sol
+++ contracts/PeerReviewDAO.sol
@@ -3,10 +3,23 @@ pragma solidity ^0.8.0;
 
 // Peer Review DAO
 contract PeerReviewDAO {
-    
-    // Membership 
+    // List of accepted papers.
+    ProposedPaper[] acceptedPapers;
+
+    // Enum vote
+    enum VoteType {
+        PUBLISH,
+        REVISE,
+        REJECT
+    }
+
+    struct ProposedPaper {
+        string[] revisions;
+    }
+
+    // Request Revisions
 
     // Create a Proposal for Research Paper
-
+    
     // Publish proposal 
 }
